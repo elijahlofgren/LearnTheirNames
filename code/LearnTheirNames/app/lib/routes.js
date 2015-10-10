1786b9f4fe84a1f60a4f92529bf6c700b60882c3
Elijah Lofgren <elijahlofgren@gmail.com>
2015-10-10 11:34:08 -0500

    Deny access to /review if user is not logged in
    Followed example at
    https://medium.com/meteor-js/how-to-build-web-apps-ultra-fast-with-meteo
    r-iron-scaffolding-and-automatic-form-generation-11734eda8e67

--- code/LearnTheirNames/app/lib/routes.js
+++ code/LearnTheirNames/app/lib/routes.js
@@ -1,24 +1,32 @@
 
 Router.route('/', {
-  name: 'home',
-  controller: 'HomeController',
-  where: 'client'
+    name: 'home',
+    controller: 'HomeController',
+    where: 'client'
 });
 
 Router.route('/review', {
-  name: 'review',
-  controller: 'ReviewController',
-  where: 'client'
+    name: 'review',
+    controller: 'ReviewController',
+    where: 'client'
 });
 
 Router.route('/signup', {
-  name: 'signup',
-  controller: 'SignupController',
-  where: 'client'
+    name: 'signup',
+    controller: 'SignupController',
+    where: 'client'
 });
 
 Router.route('/login', {
-  name: 'login',
-  controller: 'LoginController',
-  where: 'client'
+    name: 'login',
+    controller: 'LoginController',
+    where: 'client'
 });
+
+Router.onBeforeAction(function() {
+    if(!Meteor.user()) {
+        this.render('AccessDenied');
+    } else {
+        this.next();
+    }
+}, {only: ['review']});
